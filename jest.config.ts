e144f0b88092668e57a877ab03908ce6acae844c
Guilherme <guilhermemendesousa@gmail.com>
2021-08-17 07:27:33 -0300

    chore: add jest to lintstaged

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
