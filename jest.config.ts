b9921374e1470d3bbe2422e692db79e0208fa330
Guilherme <guilhermemendesdev@gmail.com>
2021-08-27 07:20:37 -0300

    perf: find a better way to resolve the test coverage problema

--- jest.config.ts
+++ jest.config.ts
@@ -5,9 +5,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/presentation/protocols'],
+  //find a better way to solve the problem to test coverage problem
   coveragePathIgnorePatterns: [
     "<rootDir>/src/presentation/protocols/",
-    "<rootDir>/src/presentation/controllers/signup/signup-protocols.ts"
+    "<rootDir>/src/presentation/controllers/signup/signup-protocols.ts",
+    "<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts"
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
